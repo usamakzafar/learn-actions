e21ff9d10abfd0bcffdd11510e5f5308854ced16
Usama K. Zafar <usama.zafar@glovoapp.com>
2022-10-07 17:42:16 +0200

    FIX

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ try {
 
   // URL Check
   if(new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(descriptionToCheck)) {
-    core.setOutput(true);
+    core.setOutput("pass", true);
   }
   else {
     setFailed("No url found");
